47e0b1c323ac114af0591d5712d4b8c7b2cb9c07
Jamie.M <j.mascall@hotmail.com>
2018-11-25 04:01:37 +0000

    custom file extension

--- SessionFileStorage.go
+++ SessionFileStorage.go
@@ -9,19 +9,21 @@ import (
 )
 
 type SessionFileStorage struct {
-	sessions   map[string]ISession
-	cleanTimer *time.Ticker
-	expiryMins int
-	filePath   string
+	sessions      map[string]ISession
+	cleanTimer    *time.Ticker
+	expiryMins    int
+	filePath      string
+	fileExtension string
 }
 
 func createSessionFileStorage(expiryMins int) ISessionStorage {
 	dir, _ := os.Getwd()
 	sessionFileStorage := &SessionFileStorage{
-		expiryMins: expiryMins,
-		cleanTimer: time.NewTicker(time.Duration(expiryMins) * time.Minute),
-		filePath:   fmt.Sprintf("%s/sessions", dir),
-		sessions:   make(map[string]ISession),
+		expiryMins:    expiryMins,
+		cleanTimer:    time.NewTicker(time.Duration(expiryMins) * time.Minute),
+		filePath:      fmt.Sprintf("%s/sessions", dir),
+		sessions:      make(map[string]ISession),
+		fileExtension: "sdata",
 	}
 
 	go func() {
@@ -34,10 +36,10 @@ func createSessionFileStorage(expiryMins int) ISessionStorage {
 	return sessionFileStorage
 }
 
-func isFileValid(path string) bool {
+func (s *SessionFileStorage) isFileValid(path string) bool {
 	dotSplit := strings.Split(path, ".")
 	ext := dotSplit[len(dotSplit)-1]
-	if ext == "data" {
+	if ext == s.fileExtension {
 		return true
 	}
 	return false
@@ -56,8 +58,8 @@ func (s *SessionFileStorage) Update(session ISession) {
 }
 
 func (s *SessionFileStorage) Delete(ssid string) {
-	if strings.Contains(ssid, ".data") == false {
-		ssid = ssid + ".data"
+	if strings.Contains(ssid, "."+s.fileExtension) == false {
+		ssid = ssid + "." + s.fileExtension
 	}
 
 	if s.sessions[ssid] != nil {
@@ -74,7 +76,7 @@ func (s *SessionFileStorage) Get(ssid string) ISession {
 func (s *SessionFileStorage) Clean() {
 	info, _ := ioutil.ReadDir(s.filePath)
 	for _, v := range info {
-		if name := v.Name(); isFileValid(name) {
+		if name := v.Name(); s.isFileValid(name) {
 			if timeSince := time.Now().Sub(v.ModTime()).Minutes(); int(timeSince) >= s.expiryMins {
 				s.Delete(name)
 			}
